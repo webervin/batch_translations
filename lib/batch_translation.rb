d921197819b136b540e70f46f7bfe8dc1feb8ce4
Szymon Fiedler <fidelski@gmail.com>
2010-09-16 13:43:09 +0200

    remove unnecessary comments

--- lib/batch_translation.rb
+++ lib/batch_translation.rb
@@ -1,44 +1,6 @@
 module ActionView
   module Helpers
     class FormBuilder
-      #
-      # Helper that renders globalize_translations fields
-      # on a per-locale basis, so you can use them separately
-      # in the same form and still saving them all at once
-      # in the same request.
-      #
-      # Use it like this:
-      #
-      # <h1>Editing post</h1> 
-      #
-      # <% form_for(@post) do |f| %>
-      #   <%= f.error_messages %>
-      #
-      #   <h2>English (default locale)</h2>
-      #   <p><%= f.text_field :title %></p>
-      #   <p><%= f.text_field :teaser %></p>
-      #   <p><%= f.text_field :body %></p>
-      #
-      #   <hr/>
-      #
-      #   <h2>Spanish translation</h2>
-      #   <% f.globalize_fields_for :es do |g| %>
-      #     <p><%= g.text_field :title %></p>
-      #     <p><%= g.text_field :teaser %></p>
-      #     <p><%= g.text_field :body %></p>
-      #   <% end %>
-      #
-      #   <hr/>
-      #
-      #   <h2>French translation</h2>
-      #   <% f.globalize_fields_for :fr do |g| %>
-      #     <p><%= g.text_field :title %></p>
-      #     <p><%= g.text_field :teaser %></p>
-      #     <p><%= g.text_field :body %></p>
-      #   <% end %>
-      #  
-      # <% end %>
-      #
       def globalize_fields_for(locale, *args, &proc)
         raise ArgumentError, "Missing block" unless block_given?
         @index = @index ? @index + 1 : 1
@@ -71,7 +33,6 @@ module Globalize
             I18n.translated_locales.reject{|key| key == :root }.each do |locale|
               translation = self.translations.find_by_locale locale.to_s
               if translation.nil?
-                # logger.debug "Building empty translation with locale '#{locale}'"
                 translations.build :locale => locale
                 save
               end
