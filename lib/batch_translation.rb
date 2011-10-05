bd7c727956e065164eb3b18150e0144684bd4e0f
Ervin Weber <webervin@gmail.com>
2011-10-05 16:34:44 +0300

    Support for SimpleForm if available

--- lib/batch_translation.rb
+++ lib/batch_translation.rb
@@ -8,7 +8,11 @@ module ActionView
         object = @object.translations.find_by_locale locale.to_s
         @template.concat @template.hidden_field_tag("#{object_name}[id]", object ? object.id : "")
         @template.concat @template.hidden_field_tag("#{object_name}[locale]", locale)
-        @template.fields_for(object_name, object, *args, &proc)
+        if @template.respond_to? :simple_fields_for
+          @template.simple_fields_for(object_name, object, *args, &proc)
+        else
+          @template.fields_for(object_name, object, *args, &proc)
+        end
       end
     end
   end
